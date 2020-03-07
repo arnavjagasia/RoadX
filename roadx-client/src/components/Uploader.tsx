d885c9fb321fc7e406c7d5e8c994f8d0324b5d74
arnavjagasia <jagasiaa@wharton.upenn.edu>
2020-03-07 12:59:18 -0500

    Upload works

--- roadx-client/src/components/Uploader.tsx
+++ roadx-client/src/components/Uploader.tsx
@@ -4,7 +4,26 @@ import { useDropzone } from 'react-dropzone'
 const uploadString: string = "Drag Roadx files here or click to select"
 function GetDropzoneUploader() {
   const onDrop = useCallback(acceptedFiles => {
-    console.log("Accepted Files", acceptedFiles);
+    // TODO handle only one file
+    const file: {[key: string]: string} = acceptedFiles[0];
+    const path: string = file['path'];
+    console.log(" Uploaded File ", acceptedFiles);
+    const requestData = {
+      'imageFilePath': path,
+      "deviceId": 123,
+      "timestamp": 123,
+    }
+
+    fetch('http://localhost:5000/create', {
+      method: 'post',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestData)
+    }).then(response => {
+      console.log(response)
+    })
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
