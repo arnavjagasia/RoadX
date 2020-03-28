8c3e4a1dd9d54ccafe2db5f20664c6093ac223c0
Adele Li <adeleli@Adeles-MacBook-Pro-2.local>
2020-03-28 18:43:20 -0400

    uploader change

--- roadx-client/src/App.tsx
+++ roadx-client/src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import './App.css';
-//import Uploader from './components/Uploader';
+import Uploader from './components/Uploader';
 import Map from './components/map';
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const App: React.FC = () => {
   return (
    <div className="App">
-     <Map />
+     <Uploader/>
    </div>
   );
 }
